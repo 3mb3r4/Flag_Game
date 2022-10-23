a3369dde8c9a2709ea4d34fdc1ca57041afb41ff
Evelina <evelina.medvedev@gmail.com>
2022-10-23 12:58:58 +0300

    screen

--- Screen.py
+++ Screen.py
@@ -19,13 +19,15 @@ def draw_mines_and_lines():
 def visualize_grid():
     line_color = const.BACKGROUND_COLOR
     pixel_x = 0
-    pixel_y = 20
+    pixel_y = 0
     pixel_x_end = 1000
-    while pixel_y != 500:
+    pixel_y_end =500
+    while pixel_y != 520:
         pygame.draw.line(screen, line_color, (pixel_x, pixel_y), (pixel_x_end, pixel_y))
         pixel_y += 20
-    while pixel_x != 1000:
-        pygame.draw.line(screen, line_color, (pixel_x, pixel_y), (pixel_x_end, pixel_y))
+    while pixel_x != 1020:
+        pygame.draw.line(screen, line_color, (pixel_x, pixel_y), (pixel_x, pixel_y_end))
+        pixel_x += 20
 
 
 
